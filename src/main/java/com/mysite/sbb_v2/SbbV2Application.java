8502327b0100d6fcf98dea89c3f258762510d225
Hjun1004 <hjun1004@naver.com>
2023-03-26 01:52:15 +0900

    setting

---
+++ src/main/java/com/mysite/sbb_v2/SbbV2Application.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/sbb_v2/SbbV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
