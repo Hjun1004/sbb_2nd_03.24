a4b5a901f7c9a97046fcf63b78e568c2d53d35e3
Hjun1004 <hjun1004@naver.com>
2023-03-26 21:17:31 +0900

    답변등록 완료

--- src/main/java/com/mysite/sbb_v2/answer/answerController/AnswerController.java
+++ src/main/java/com/mysite/sbb_v2/answer/answerController/AnswerController.java
@@ -1,7 +1,33 @@
 package com.mysite.sbb_v2.answer.answerController;
 
+import com.mysite.sbb_v2.answer.service.AnswerService;
+import com.mysite.sbb_v2.question.entity.Question;
+import com.mysite.sbb_v2.question.questionRepository.QuestionRepository;
+import com.mysite.sbb_v2.question.service.QuestionService;
+import groovyjarjarpicocli.CommandLine;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Getter
+@Setter
+@RequiredArgsConstructor
+@RequestMapping("/2answer")
 public class AnswerController {
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, String content){
+        Question q = this.questionService.getQuestion(id);
+        this.answerService.create(q, content);
+        return "redirect:/2question/detail/%d".formatted(id);
+    }
 }
--- src/main/java/com/mysite/sbb_v2/answer/service/AnswerService.java
+++ src/main/java/com/mysite/sbb_v2/answer/service/AnswerService.java
@@ -1,4 +1,24 @@
 package com.mysite.sbb_v2.answer.service;
 
+import com.mysite.sbb_v2.answer.answerRepository.AnswerRepository;
+import com.mysite.sbb_v2.answer.entity.Answer;
+import com.mysite.sbb_v2.question.entity.Question;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Service
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    public void create(Question q, String content){
+        Answer a = new Answer();
+        a.setQuestion(q);
+        a.setContent(content);
+        this.answerRepository.save(a);
+    }
 }
